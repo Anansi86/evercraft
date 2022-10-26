ff01cc21c067796a11b8a8595a36e26baf8518c9
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-10-26 14:58:42 +0000

    abilities

--- src/evercraft.py
+++ src/evercraft.py
@@ -1,32 +1,46 @@
 
 
-class Character:
-    def __init__(self,name,align,armorClass,hitPoints):
+class Character():
+    def __init__(self, name, align):
         self.name = name
         self.align = align
-        self.armorClass = armorClass
-        self.hitPoints  = hitPoints
+        self.armorClass = 10
+        self.hitPoints = 5
         self.is_dead = False
+        self.strength = 10
+        self.dexterity = 10
+        self.constitution = 10
+        self.wisdom = 10
+        self.intelligence = 10
         
     # Charecter Can attack
     # attack greater than or equal to  enemy armorClass
-    #if attack = 20 always hit
-    def attack(self,diceroll,enemyArmor):
+    # if attack = 20 always hit
+    def attack(self, diceroll, enemyArmor,hitPoints):
         if diceroll == 20:
             return 'Critical Hit'
         elif diceroll >= enemyArmor:
             return True
-        else: 
+        else:
             return False
-    
-    #damaged
-    #if attack == true
-        #enemy gets 1 hp
+
+    # damaged
+    # if attack == true
+        # enemy gets 1 hp
     # else if diceroll is 20
 
-    def damaged(self,diceroll,armorClass):
+    def damaged(self, diceroll, armorClass):
         if diceroll >= self.armorClass:
             return self.hitPoints - 1
         else:
-             return self.hitPoints
+            return self.hitPoints
 
+
+"""
+Charecter has:
+Strength, Dexterity, Constitution, 
+wisdom, intelligence, 
+Charisma
+
+Create class that contains abilities that the charecter can inheret
+"""
--- src/tests/test_iteration1.py
+++ src/tests/test_iteration1.py
@@ -1,33 +1,86 @@
 from evercraft import *
 
+
 def test_character():
-    blodgram = Character('blodgram','good',10,5)
+    blodgram = Character('blodgram', 'good')
     assert isinstance(blodgram, Character)
 
 
 def test_char_name():
-    blodgram = Character('blodgram','good',10,5)
+    blodgram = Character('blodgram', 'good')
     assert blodgram.name != 'alex'
 
+
 def test_char_align():
-    blodgram = Character('blodgram','good',10,5)
+    blodgram = Character('blodgram', 'good')
     assert blodgram.align == 'good'
 
+
 def test_armor():
-    blodgram = Character('blodgram','good',10,5)
+    blodgram = Character('blodgram', 'good')
     assert blodgram.armorClass != 9
 
+
 def test_hitPoints():
-    blodgram = Character('blodgram','good',10,5)
+    blodgram = Character('blodgram', 'good')
     assert blodgram.hitPoints == 5
 
+# hit
+
+
 def test_attack():
-    blodgram = Character('blodgram','good',10,5)
-    assert blodgram.attack(11, 10) == True #hit
+    blodgram = Character('blodgram', 'good')
+    assert blodgram.attack(11, 10) == True  # hit
+
+# nothit
+
+
+def test_no_hit():
+    blodgram = Character('blodgram', 'good')
+    assert blodgram.attack(8, 10) == False
+
+
+# Critical hit test
+def test_critical_hit():
+    blodgram = Character('blodgram', 'good')
+    assert blodgram.attack(20, 10) == 'Critical Hit'
+
 
 def test_damaged():
-    blodgram = Character('blodgram','good',10,5)
-    mario = Character('mario','bad',11,5)
-    assert blodgram.damaged(10,mario.armorClass) == 4
+    blodgram = Character('blodgram', 'good')
+    mario = Character('mario', 'bad')
+    assert blodgram.damaged(10, mario.armorClass) == 4
 
 
+def test_is_dead():
+    blodgram = Character("blodgram", 'good')
+    assert blodgram.is_dead != True
+
+
+def test_is_alive():
+    blodgram = Character("blodgram", 'good')
+    assert blodgram.is_dead == False
+
+
+def test_strenth():
+    blodgram = Character('Blodgram', 'good')
+    assert blodgram.strength == 10
+
+
+def test_dexterity():
+    blodgram = Character('Blodgram', 'good')
+    assert blodgram.dexterity == 10
+
+
+def test_constitution():
+    blodgram = Character('Blodgram', 'good')
+    assert blodgram.constitution == 10
+
+
+def test_wisdom():
+    blodgram = Character('Blodgram', 'good')
+    assert blodgram.wisdom == 10
+
+def test_intelligence():
+    blodgram = Character('Blodgram', 'good')
+    assert blodgram.intelligence == 10
